d5ccd919d0f16a202df3d7bb473c76b343e7df7b
xentare <H4113@student.jamk.fi>
2016-01-21 22:24:54 +0200

    New layout

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,13 @@ $( document ).ready(function(){
         $('#imagemodal').modal('show');
     });
 
+    $('ul li a').click(function (e) {
+        var href = $(e.target).attr('href');
+        href = href.replace('#', '');
+        var target = $('[data-link=\'' + href + '\']');
+        $('body, html').animate({
+            scrollTop: target.offset().top - 70
+        }, 400);
+    });
+
 });
